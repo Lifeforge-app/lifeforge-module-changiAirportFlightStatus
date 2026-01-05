6145a2ec1c4c97440a727881b8028d65c8f866a0
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 12:06:56 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,10 +2,7 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Changi Airport Flight Status',
-  icon: 'tabler:plane',
   routes: {
     '/': lazy(() => import('@'))
   },
-  category: 'Information'
 } satisfies ModuleConfig
